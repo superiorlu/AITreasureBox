a6a31a7ae0e5926e7b27b4c3f7aa3370ba33f2f2
superiorlu <luyingrui518@gmail.com>
2023-03-22 19:02:36 +0800

    update readme srcript

--- update_readme.rb
+++ update_readme.rb
@@ -22,8 +22,7 @@ def update_readme(start_str, end_str, file_name, repo_stars)
                        repo_stars[match[1]]
                      end
         repo_stars[match[1]] = star_count
-        repo_info = repo_info.match(/\[(.*?)\].*/)
-        repo = { repo_info: repo_info[0], desc: desc, star_count:  star_count, original_index: index - 2 }
+        repo = { repo_info: repo_info, desc: desc, star_count:  star_count, original_index: index - 2 }
         repos << repo
     end
   end
@@ -33,7 +32,7 @@ def update_readme(start_str, end_str, file_name, repo_stars)
   repos.sort_by!{ |r| -r[:star_count] }
   repos.each_with_index do |repo, index|
     now_index = index + 1
-    line = format("| %i|%s %s|%s|\n", now_index, arrow_style(file_name, repo[:original_index], now_index), repo[:repo_info], repo[:desc])
+    line = format("|%s%i|%s|%s|\n", arrow_style(file_name, repo[:original_index], now_index), now_index, repo[:repo_info], repo[:desc])
     new_readme << line
   end
   new_readme << lines[end_index..-1].join
@@ -43,11 +42,11 @@ end
 # 计算arrow样式
 def arrow_style(file_name, original_index, now_index)
   return nil if now_index == original_index
-  style = ''
+  style = ' '
   if file_name == 'README.md'
-    style = now_index < original_index ? '<span class="red-up-arrow"></span>' : '<span class="green-down-arrow"></span>'
+    style = now_index < original_index ? '<span style="color: red;">&#x2191;</span>' : '<span style="color: green;">&#x2193;</span>'
   else
-    style = now_index < original_index ? '<span class="green-up-arrow"></span>' : '<span class="red-down-arrow"></span>'
+    style = now_index < original_index ? '<span style="color: green;">&#x2191;</span>' : '<span style="color: red;">&#x2193;</span>'
   end
   style
 end
